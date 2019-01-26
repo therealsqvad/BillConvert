a2db1ce4a19aa0b9b385839b5fbfae8234a6bae2
therealsqvad <nikita_klepov@mail.ru>
2019-01-26 19:52:47 +0700

    add check inputed

--- app/src/main/java/ru/klepovnikita/billconvert/MainActivity.java
+++ app/src/main/java/ru/klepovnikita/billconvert/MainActivity.java
@@ -23,7 +23,7 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] data = {"ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM",
+    String[] bills = {"ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM",
             "BBD", "BDT","BGN","BHD","BIF","BND","BOB",
             "BRL","BSD","BTC","BTN","BWP","BYN","BYR",
             "BZD","CAD","CDF","CHF","CLP","CNY","COP",
@@ -39,9 +39,8 @@ public class MainActivity extends AppCompatActivity {
     String source;
     String result;
     Button btnConvert;
-    TextView textView;
-    TextView intext;
-    TextView outtext;
+    TextView sourceText;
+    TextView resultText;
 
     private static final String TAG = "RESPLOG";
 
@@ -50,27 +49,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnConvert = (Button) findViewById(R.id.btnConvert);
-        textView = (TextView) findViewById(R.id.textView3);
-        intext = (EditText) findViewById(R.id.sourceValue);
-        outtext = (EditText) findViewById(R.id.resultValue);
+        sourceText = (EditText) findViewById(R.id.sourceValue);
+        resultText = (EditText) findViewById(R.id.resultValue);
 
         // адаптер
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, bills);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         Spinner spinner = (Spinner) findViewById(R.id.sourceBill);
         spinner.setAdapter(adapter);
-        spinner.setSelection(2);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+        spinner.setSelection(0);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, bills);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner2 = (Spinner) findViewById(R.id.resultBill);
         spinner2.setAdapter(adapter);
-        spinner2.setSelection(0);
+        spinner2.setSelection(1);
         spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
-                source = data[position];
+                source = bills[position];
             }
             @Override
             public void onNothingSelected(AdapterView<?> arg0) {
@@ -80,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
-                result = data[position];
+                result = bills[position];
             }
             @Override
             public void onNothingSelected(AdapterView<?> arg0) {
@@ -90,39 +88,43 @@ public class MainActivity extends AppCompatActivity {
         OnClickListener oclBtnOk = new OnClickListener() {
            @Override
             public void onClick(View v) {
-               NetworkService.getInstance()
-                       .getJSONApi()
-                       .readJsonFromFileUri(source+ "_" + result, "ultra")
-                       .enqueue(new Callback<JsonObject>() {
-                           @Override
-                           public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                               Log.d(TAG, "Response: "+response);
-                               String jsonString = response.body().toString();
-                               Log.d(TAG, "jsonString: "+jsonString);
-                                double kf = Double.parseDouble(
-                                        jsonString.substring(
-                                                jsonString.indexOf(':') + 1,
-                                                jsonString.indexOf('}')
-                                        ));
-                               Gson gson = new Gson();
+               if (sourceText.getText().toString()=="") {
+                   Toast.makeText(getBaseContext(), "Не введено исходное значение", Toast.LENGTH_SHORT).show();
+               }
+               else
+               {
+                   NetworkService.getInstance()
+                           .getJSONApi()
+                           .readJsonFromFileUri(source + "_" + result, "ultra")
+                           .enqueue(new Callback<JsonObject>() {
+                               @Override
+                               public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                                   Log.d(TAG, "Response: " + response);
+                                   String jsonString = response.body().toString();
+                                   Log.d(TAG, "jsonString: " + jsonString);
+                                   double kf = Double.parseDouble(
+                                           jsonString.substring(
+                                                   jsonString.indexOf(':') + 1,
+                                                   jsonString.indexOf('}')
+                                           ));
+                                   Gson gson = new Gson();
 
-                               Log.d(TAG, "jsonString: "+jsonString);
-                               double res = kf*(Double.parseDouble(intext.getText().toString()));
+                                   Log.d(TAG, "jsonString: " + jsonString);
+                                   double res = kf * (Double.parseDouble(sourceText.getText().toString()));
 
-                                outtext.setText(String.format("%.3f", res));
-                               Log.i(TAG, String.valueOf(kf));
+                                   resultText.setText(String.format("%.3f", res));
+                                   Log.i(TAG, String.valueOf(kf));
 
-                           }
+                               }
 
-                           @Override
-                           public void onFailure(Call<JsonObject> call, Throwable t) {
-                               Toast.makeText(getBaseContext(), "Ошибка при подключении", Toast.LENGTH_SHORT).show();
-                               t.printStackTrace();
-                           }
-                       });
+                               @Override
+                               public void onFailure(Call<JsonObject> call, Throwable t) {
+                                   Toast.makeText(getBaseContext(), "Ошибка! Проверьте подключение к сети", Toast.LENGTH_SHORT).show();
+                                   t.printStackTrace();
+                               }
+                           });
 
-
-
+               }
 
             }
         };
